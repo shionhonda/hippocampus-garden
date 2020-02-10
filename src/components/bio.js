7e68c58d865c432d24d361bf6ba99dcc15fca5af
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2020-02-10 19:40:24 +0900

    change font

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,12 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
+        Written by <strong>{author}</strong>.
+        Follow me on {` `}
+        <a href={`https://twitter.com/${social.twitter}`}>Twitter
+        </a>!
       </p>
     </div>
   )
