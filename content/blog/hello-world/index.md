a0515d03183ff136469c339b053cfac6d59ac9d6
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2020-02-10 13:23:03 +0900

    First commit

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -4,9 +4,7 @@ date: "2015-05-01T22:12:03.284Z"
 description: "Hello World"
 ---
 
-This is my first post on my new fake blog! How exciting!
-
-I'm sure I'll write a lot more interesting things in the future.
+Hello, world! This is my first post on my new blog!
 
 Oh, and here's a great quote from this Wikipedia on
 [salted duck eggs](http://en.wikipedia.org/wiki/Salted_duck_egg).
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Hippocampus's Garden`,
+    author: `Shion Honda`,
+    description: `Machine learning, data science, programming, and sometimes music.`,
+    siteUrl: `https://hippocampus-garden.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `shion_honda`,
     },
   },
   plugins: [
