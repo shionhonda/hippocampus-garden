2359df21f8c4097dabaaa76615a8ed075fd0738a
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2020-02-21 22:20:00 +0900

    external links

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,9 +19,31 @@ const About = ({ data, location }) => {
             <img src="https://media-exp1.licdn.com/dms/image/C5103AQGDS2utMiobKQ/profile-displayphoto-shrink_200_200/0?e=1587600000&v=beta&t=_HITxpVXkoPWiGKZsMKBLFUVlbZwrxWhF7noPpfcBXc"
             alt="profile" width="40%" ></img>
           </div>
-          <p>See <a href="https://www.linkedin.com/in/shionhonda/">LinkedIn</a> for my education and work experiences
-          and <a href="https://scholar.google.co.jp/citations?user=NhNlsZcAAAAJ">Google Scholar</a> for publications.</p>
+          {/* <div style={{ textAlign:"center"}}>
+            <img src=""
+            alt="profile" width="40%" ></img>
+          </div> */}
+          <p>See <a href="https://www.linkedin.com/in/shionhonda/" target="_blank" rel="noopener">LinkedIn</a> for my education and work experiences
+          and <a href="https://scholar.google.co.jp/citations?user=NhNlsZcAAAAJ" target="_blank" rel="noopener">Google Scholar</a> for publications.</p>
 
+          <h1>External links</h1>
+          You may also like:
+          <ul>
+            <li>
+              <a href="https://qiita.com/shionhonda" target="_blank" rel="noopener"><strong>Qiita</strong></a> (all in Japanese).
+              I used to write survey posts here. The topics are somewhat wide: system trading for cryptocurrencies, graph neural networks, deep reinforcement learning, deep generative models, etc.
+            </li>
+            <li>
+              <a href="https://ai-scholar.tech/author/honda-shion/" target="_blank" rel="noopener"><strong>AI-SCHOLAR</strong></a> (all in Japanese).
+              I contributed some introductory articles to a web media for non-experts in AI.
+              Popular posts are on MuZero, XLNet, and NGBoost.
+            </li>
+            <li>
+              <a href="https://www.slideshare.net/ShionHonda" target="_blank" rel="noopener"><strong>SlideShare</strong></a> (mostly in Japanese).
+              Here are some slides that I used in my talks and presentations.
+            </li>
+          </ul>
+          
           <h1>News</h1>
           <ul></ul>   
         </div>
