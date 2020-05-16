381f4a1ae7f7e6f43cc07828790e0c3f016e22bc
Shion Honda <26x.orc.ed5.1hs@gmail.com>
2020-05-16 18:17:13 +0900

    find function in popular posts

--- src/components/popular.js
+++ src/components/popular.js
@@ -4,7 +4,7 @@ import Image from "../components/image"
 import { rhythm, scale } from "../utils/typography"
 
 const PopularPost = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
         query allPagesAndViews {
             allMarkdownRemark(limit: 1000, sort: {order: DESC, fields: frontmatter___date}) {
                 edges {
@@ -30,44 +30,48 @@ const PopularPost = () => {
         }
     `)
 
-    const allPosts = data.allMarkdownRemark.edges;
-    const appPageViews = data.allPageViews;
-    const results = [];
+  const allPosts = data.allMarkdownRemark.edges;
+  const popularPosts = data.allPageViews.edges;
+  const results = [];
 
-    appPageViews.edges
-    .forEach(({ node }) => {
-      allPosts.forEach(({ node: post }) => {
-        if (post.fields.slug === node.id) {
-          results.push({
-            count: node.totalCount,
-            ...post,
-          });
-        }
+  for (const a of popularPosts) {
+    const popularPost = allPosts.find(b => b.node.fields.slug === a.node.id);
+    if (popularPost == null) {
+      continue;
+    } else {
+      results.push({
+        count: a.node.totalCount,
+        ...popularPost.node,
       });
-    });
-    console.log(results);
+    }
+    if (results.length >= 5) {
+      break;
+    }
+  };
 
-    return (
-        <div style={{marginBottom: rhythm(0.5), backgroundColor:"white", padding: rhythm(0.5)}}>
-            <h3>Popular Posts</h3>
-          {results.slice(0,5).map(result => (
-            <article key={result.id} >
-              <Link  style={{boxShadow: `none`, textDecoration: `none`, color: `inherit`,}}　to={result.fields.slug}>
-                  <div style={{display:"flex", flexFlow: "row", 
-                                marginTop: rhythm(0.5)}}>
-                    <div style={{width:"120px", paddingRight: rhythm(0.5)}}>
-                        <Image filename={result.frontmatter.featuredImage}/>
-                    </div>
-                    <small style={{width:"150px",}}>
-                        {result.frontmatter.title}                  
-                    </small>   
-                  </div>
-                       
-              </Link>
-            </article>
-          ))}
-        </div>
-    )
+  return (
+    <div style={{ marginBottom: rhythm(0.5), backgroundColor: "white", padding: rhythm(0.5) }}>
+      <h3>Popular Posts</h3>
+      {results.map(result => (
+        <article key={result.id} >
+          <Link style={{ boxShadow: `none`, textDecoration: `none`, color: `inherit`, }} to={result.fields.slug}>
+            <div style={{
+              display: "flex", flexFlow: "row",
+              marginTop: rhythm(0.5)
+            }}>
+              <div style={{ width: "120px", paddingRight: rhythm(0.5) }}>
+                <Image filename={result.frontmatter.featuredImage} />
+              </div>
+              <small style={{ width: "150px", }}>
+                {result.frontmatter.title}
+              </small>
+            </div>
+
+          </Link>
+        </article>
+      ))}
+    </div>
+  )
 };
 
 export default PopularPost
